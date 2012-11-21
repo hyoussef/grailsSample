95e2b6a138b8de0d9169f4d94e21ccdacf221284
YOUSSEF <i060356@lvlm60205400a.dhcp.par.sap.corp>
2012-11-21 14:40:30 +0100

    first commit

---
+++ sample/grails-app/conf/BootStrap.groovy
@@ -0,0 +1,20 @@
+import com.sample.Role
+import com.sample.User
+import com.sample.UserRole
+class BootStrap {
+
+   def init = { servletContext ->
+
+      def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+      def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+      def testUser = new User(username: 'me', enabled: true, password: 'password')
+      testUser.save(flush: true)
+
+      UserRole.create testUser, adminRole, true
+
+      assert User.count() == 1
+      assert Role.count() == 2
+      assert UserRole.count() == 1
+   }
+}
---
+++ sample/grails-app/controllers/com/sample/SecureController.groovy
@@ -0,0 +1,9 @@
+package com.sample
+import grails.plugins.springsecurity.Secured
+
+//@Secured(['ROLE_ADMIN'])
+class SecureController {
+   def index = {
+      render 'Secure access only'
+   }
+}
